8550442e3600aa85f871a2fb8c2acda2e6d9f664
John Abanes <john.abanes@nyu.edu>
2024-07-12 14:25:04 -0400

    update setup.sh to work with all Insta cameras

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@
 # sudo usbip attach -r 127.0.0.1 -b 4-1
 
 #Add permissions for USB port
-#TODO: Change to venderID = 0x2e1a
-echo SUBSYSTEM=='"usb"', ATTR{product}=='"Insta360 ONE"', SYMLINK+='"insta"' | sudo tee /etc/udev/rules.d/99-insta.rules
+# udevadm info --name /dev/bus/usb/003/026 --attribute-walk
+echo SUBSYSTEM=='"usb"', ATTR{idVendor}=='"2e1a"', SYMLINK+='"insta"' | sudo tee /etc/udev/rules.d/99-insta.rules
 
 #Trigger SYMLINK creation
 sudo udevadm trigger
