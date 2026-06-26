fe5a93833c0b91fcd96f12f78626d8c13cb8bdc6
UseVix <jozef.franciszek.wiatr@gmail.com>
2026-06-26 14:23:48 +0200

    Revert "cleanup" - This branch is called bundle and cleanup commit have changed .h to .hpp in includes which causes the package to not work with humble distro. This branch shall be merged to humble branch in main repo.
    This reverts commit a621a43a0ad108ce481e1fbe0efe87a7ad733734.

--- include/equirectangular.hpp
+++ include/equirectangular.hpp
@@ -3,7 +3,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
-#include <cv_bridge/cv_bridge.hpp>
+#include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
 #include <memory>
 #include <mutex>
