9d50ed4fbaf97c1ffc89a2005683a5a85af91791
avasalya <asheshvasalya@gmail.com>
2026-08-04 15:31:28 +0900

    use 1152x1152 resolution and skip imu filter

--- src/main.cpp
+++ src/main.cpp
@@ -127,7 +127,7 @@ public:
 
         cam->SyncLocalTimeToCamera(utc_time,offset_time);       
         ins_camera::LiveStreamParam param;
-        param.video_resolution = ins_camera::VideoResolution::RES_1920_960P30; //Change this line to edit the resolution
+        param.video_resolution = ins_camera::VideoResolution::RES_1152_1152P30; //Change this line to edit the resolution
         //Possible resolutions (results may vary per model) are:
         //RES_3840_1920P30
         //RES_2560_1280P30
